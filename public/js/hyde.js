885f47f623fafd9060d7a2f7d4422269acd99868
Ma Yanbin <myanbin@gmail.com>
2019-04-01 11:37:54 +0800

    update scrollbar

--- public/js/hyde.js
+++ public/js/hyde.js
@@ -5,23 +5,8 @@
 
 console.log("This Jekyll theme's author is Ma Yanbin <myanbin@gmail.com>");
 
-
 $(function() {
 
-  if (navigator.platform.match(/win32/i)) {
-    var cssId = '_webkit-scrollbar';
-    if (!document.getElementById(cssId)) {
-        var head = document.getElementsByTagName('head')[0];
-        var style = document.createElement('style');
-        style.id = cssId;
-        style.innerText = `
-        ::-webkit-scrollbar { width: 6px; height: 6px; background-color: #fff; }
-        ::-webkit-scrollbar-track { background-color: #fff; }
-        ::-webkit-scrollbar-thumb { background-color: #ccc; border: 1px solid #ddd; }
-        `;
-        head.appendChild(style);
-    }
-  }
   /**
    * dark mode
    */
